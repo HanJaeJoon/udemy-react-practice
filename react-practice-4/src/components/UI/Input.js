bcf5d08242f740444255da580c5436a5e7d601a6
jaejoon.han <jaejoon.han@crevisse.com>
2023-07-06 19:01:18 +0900

    react-practice-4 useContext, useRef

--- react-practice-4/src/components/UI/Input.js
+++ react-practice-4/src/components/UI/Input.js
@@ -1,13 +1,15 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       {/* for는 예약어라서 htmlFor 사용(class를 ClassName으로 사용하는 것처럼) */}
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
