50cf7d0d799a7e202edb19177b799402300fcac8
jaejoon.han <jaejoon.han@crevisse.com>
2023-07-07 12:07:40 +0900

    react-practice-4 useEffect

--- react-practice-4/src/components/Layout/HeaderCartButton.js
+++ react-practice-4/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
 
   const numberOfCartItems = cartContext.items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  const { items } = cartContext;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- react-practice-4/src/store/CartProvider.js
+++ react-practice-4/src/store/CartProvider.js
@@ -19,8 +19,6 @@ const cartReducer = (state, action) => {
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
-    console.log(existingCartItem)
-
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
@@ -41,6 +39,29 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItemsTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedItemsTotalAmount,
+    };
   } else if (action.type === 'CLEAR') {
     return defaultCartState;
   }
